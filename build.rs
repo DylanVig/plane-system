8be9e690dbdb9498ed735896dcb187acbf786513
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-09-05 13:07:11 -0400

    it compiles

---
+++ build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    cxx_build::bridge("src/main.rs")
+        .include("vendor/libsoccptp/include")
+        .include("vendor/libsoccptp/ports")
+        .file("vendor/libsoccptp/ports/ports_usb_impl.cpp")
+        .file("vendor/libsoccptp/ports/ports_ptp_impl.cpp")
+        .file("vendor/libsoccptp/sources/parser.cpp")
+        .file("vendor/libsoccptp/sources/socc_ptp.cpp")
+        .flag_if_supported("-std=c++11")
+        .compile("soccptp");
+
+    println!("cargo:rerun-if-changed=src/main.rs");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cxx::bridge]
+mod ffi {
+    extern "C" {
+        include!("socc_types.h");
+
+        type socc_device_handle_info_t;
+    }
+
+    extern "Rust" {}
+}
