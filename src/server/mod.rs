7a587ebbec9136753692cb51d54712e8513167eb
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-10-03 15:19:40 -0400

    new server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,7 @@
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
-use tide::{self, Request};
+use warp::{self, Filter};
 
 use crate::state::RegionOfInterest;
 
@@ -28,45 +28,19 @@ enum ClientType {
 pub async fn serve() -> Result<(), std::io::Error> {
     info!("initializing server");
 
-    let state = ServerState {};
-
-    let mut app = tide::with_state(state);
-
-    app.at("/").get(|_| async {
-        let response = tide::Response::builder(200)
-            .body(tide::Body::empty())
-            .content_type("text/plain")
-            .build();
-        Ok(response)
-    });
-
-    app.at("/connect")
-        .get(|req: Request<ServerState>| async move {
-            todo!();
-            Ok(tide::Response::new(200))
-        });
-
-    app.at("/disconnect")
-        .get(|req: Request<ServerState>| async move {
-            todo!();
-            Ok(tide::Response::new(200))
-        });
-
-    let mut api = app.at("/api");
-
-    api.at("/add-roi")
-        .post(|mut req: Request<ServerState>| async move {
-            let body: AddROIs = req.body_json().await?;
-
+    let add_roi = warp::path!("api" / "add-roi")
+        .and(warp::post())
+        .and(warp::body::json())
+        .map(move |body: AddROIs| {
             debug!("received ROIs: {:?}", &body);
-
-            Ok(tide::Response::new(200))
+            warp::reply()
         });
 
-    let address = "127.0.0.1:8080";
+    let address = ([127, 0, 0, 1], 8080);
     info!("initialized server");
-    info!("listening at {}", address);
+    info!("listening at {:?}", address);
 
-    app.listen(address).await?;
+    warp::serve(add_roi).run(address).await;
+
     Ok(())
 }
