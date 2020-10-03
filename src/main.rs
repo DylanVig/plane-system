a35309eaeb595dd561c6164e9f131dc5ca0e9cca
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-10-03 13:57:40 -0400

    Delete camera test code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{rc::Rc, sync::atomic::AtomicBool, sync::atomic::Ordering, sync::Arc};
+use std::{sync::atomic::AtomicBool, sync::atomic::Ordering, sync::Arc};
 
-use camera::interface::CameraInterface;
 use pixhawk::state::PixhawkMessage;
 use tokio::sync::broadcast;
 
@@ -31,45 +30,29 @@ async fn main() -> anyhow::Result<()> {
 
     let loop_continue = Arc::new(AtomicBool::new(true));
 
-    // let pixhawk_task = tokio::spawn({
-    //     let loop_continue = loop_continue.clone();
+    let pixhawk_task = tokio::spawn({
+        let loop_continue = loop_continue.clone();
 
-    //     async move {
-    //         info!("connecting to pixhawk");
+        async move {
+            info!("connecting to pixhawk");
 
-    //         // pixhawk telemetry should be exposed on localhost:5763 for SITL
-    //         // TODO: add case for when it's not the SITL
-    //         let mut client = pixhawk::client::PixhawkClient::connect(":::5763").await?;
+            // pixhawk telemetry should be exposed on localhost:5763 for SITL
+            // TODO: add case for when it's not the SITL
+            let mut client = pixhawk::client::PixhawkClient::connect(":::5763").await?;
 
-    //         info!("initializing pixhawk");
-    //         client.init().await?;
+            info!("initializing pixhawk");
+            client.init().await?;
 
-    //         while loop_continue.load(Ordering::Relaxed) {
-    //             let msg = client.recv().await?;
-    //             trace!("received message: {:?}", msg);
-    //         }
+            while loop_continue.load(Ordering::Relaxed) {
+                let msg = client.recv().await?;
+                trace!("received message: {:?}", msg);
+            }
 
-    //         Result::<(), anyhow::Error>::Ok(())
-    //     }
-    // });
+            Result::<(), anyhow::Error>::Ok(())
+        }
+    });
 
-    // let _ = pixhawk_task.await?;
-
-    let mut camera = CameraInterface::new();
-
-    info!("connecting to camera");
-
-    camera.connect()?;
-
-    info!("taking photo");
-
-    camera.take_photo()?;
-
-    info!("disconnecting from camera");
-
-    camera.disconnect()?;
-
-    info!("done");
+    let _ = pixhawk_task.await?;
 
     Ok(())
 }
