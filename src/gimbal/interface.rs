271d6f433d3ba59548440c2b24ce3e75c73c6535
Evan Patrick <evandp1999@gmail.com>
2021-01-25 16:34:17 -0500

    making progress with interface compatibilities with other crates

--- src/gimbal/interface.rs
+++ src/gimbal/interface.rs
@@ -2,33 +2,40 @@ use num_traits::FromPrimitive;
 use simplebgc::*;
 use std::io::{Read, Write};
 use std::time::Duration;
+use tokio_serial::{SerialPortSettings, Serial};
+use tokio_util::codec::{Decoder, Encoder, Framed};
+use tokio::io::{AsyncRead, AsyncWrite};
+use futures::{Sink, SinkExt, Stream, StreamExt};
 
 const SBGC_VID: u16 = 0x10C4;
 const SBGC_PID: u16 = 0xEA60;
+const FTDI_VID: u16 = 0x0403;
+const FTDI_PID: u16 = 0x6001;
 
 pub struct GimbalInterface {
-    port: serialport::TTYPort,
+    // TODO second type arg can be a dyn trait over both types
+    inner: Framed<Serial, V1Codec>,
 }
 
 impl GimbalInterface {
     pub fn new() -> anyhow::Result<Self> {
-        if let Some(device_name) = Self::find_usb_device_name()? {
-            let port = serialport::new(device_name, 115_200)
-                .timeout(Duration::from_millis(10))
-                .open_native()?;
+        if let Some(device_path) = Self::find_usb_device_path()? {
+            let settings = SerialPortSettings::default();
+            let port = Serial::from_path(device_path, &settings)?;
 
-            return Ok(Self { port });
+            return Ok(Self { inner: V1Codec.framed(port) });
         } else {
             return Err(anyhow!("SimpleBGC usb device not found"));
         }
     }
 
-    fn find_usb_device_name() -> anyhow::Result<Option<String>> {
+    fn find_usb_device_path() -> anyhow::Result<Option<String>> {
         let ports = serialport::available_ports()?;
+        info!("{:?}", ports);
         for port in ports {
             match port.port_type {
                 serialport::SerialPortType::UsbPort(info) => {
-                    if info.vid == SBGC_VID && info.pid == SBGC_PID {
+                    if (info.vid == SBGC_VID && info.pid == SBGC_PID) || (info.vid == FTDI_VID && info.pid == FTDI_PID) {
                         return Ok(Some(port.port_name));
                     }
                 }
@@ -38,47 +45,48 @@ impl GimbalInterface {
         Ok(None)
     }
 
-    fn send_command(&mut self, cmd: OutgoingCommand) -> anyhow::Result<()> {
+    async fn send_command(&mut self, cmd: OutgoingCommand) -> anyhow::Result<()> {
         let bytes = cmd.to_v1_bytes();
-        self.port.write(&bytes[..])?;
+        self.inner.send(cmd).await?;
         Ok(())
     }
 
-    fn get_response(&mut self) -> anyhow::Result<IncomingCommand> {
-        let mut buf: Vec<u8> = vec![0; 4096];
-        let marker = self.port.read(buf.as_mut_slice())?;
-        let (cmd, _) = IncomingCommand::from_v1_bytes(&buf[..marker])?;
-        Ok(cmd)
-    }
-
-    pub fn control_angles(&mut self, mut roll: f64, mut pitch: f64) -> anyhow::Result<()> {
-        info!("Got request for {}, {}", roll, pitch);
-        if roll.abs() > 50.0 || pitch.abs() > 50.0 {
-            roll = 0.0;
-            pitch = 0.0;
+    async fn get_response(&mut self) -> anyhow::Result<Option<IncomingCommand>> {
+        if let Some(cmd_result) = self.inner.next().await {
+            let cmd = cmd_result?;
+            return Ok(Some(cmd));
         }
-
-        let factor: f64 = (2 ^ 14) as f64 / 360.0;
-
-        let command = OutgoingCommand::Control(ControlData {
-            mode: ControlFormat::Legacy(AxisControlState::from_u8(0x02).unwrap()),
-            axes: RollPitchYaw {
-                roll: AxisControlParams {
-                    /// unit conversion: SBGC units are 360 / 2^14 degrees
-                    angle: (roll * factor) as i16,
-                    speed: 1200,
-                },
-                pitch: AxisControlParams {
-                    /// unit conversion: SBGC units are 360 / 2^14 degrees
-                    angle: (pitch * factor) as i16,
-                    speed: 2400,
-                },
-                yaw: AxisControlParams { angle: 0, speed: 0 },
-            },
-        });
-        self.send_command(command)?;
-        // TODO: we need to implement CMD_CONFIRM in the simplebgc-rs crate
-        // let response = self.get_response()?;
-        Ok(())
+        Ok(None)
     }
+
+    // pub fn control_angles(&mut self, mut roll: f64, mut pitch: f64) -> anyhow::Result<()> {
+    //     info!("Got request for {}, {}", roll, pitch);
+    //     if roll.abs() > 50.0 || pitch.abs() > 50.0 {
+    //         roll = 0.0;
+    //         pitch = 0.0;
+    //     }
+
+    //     let factor: f64 = (2 ^ 14) as f64 / 360.0;
+
+    //     let command = OutgoingCommand::Control(ControlData {
+    //         mode: ControlFormat::Legacy(AxisControlState::from_u8(0x02).unwrap()),
+    //         axes: RollPitchYaw {
+    //             roll: AxisControlParams {
+    //                 /// unit conversion: SBGC units are 360 / 2^14 degrees
+    //                 angle: (roll * factor) as i16,
+    //                 speed: 1200,
+    //             },
+    //             pitch: AxisControlParams {
+    //                 /// unit conversion: SBGC units are 360 / 2^14 degrees
+    //                 angle: (pitch * factor) as i16,
+    //                 speed: 2400,
+    //             },
+    //             yaw: AxisControlParams { angle: 0, speed: 0 },
+    //         },
+    //     });
+    //     self.send_command(command)?;
+    //     // TODO: we need to implement CMD_CONFIRM in the simplebgc-rs crate
+    //     // let response = self.get_response()?;
+    //     Ok(())
+    // }
 }
