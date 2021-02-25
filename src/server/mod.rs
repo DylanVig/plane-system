7fcb2c4b3037233dcb9866aab1b08bfa505bbdf8
Jack Williamson <jew256@cornell.edu>
2021-02-25 18:21:01 -0500

    added image route

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -48,11 +48,13 @@ pub async fn serve(channels: Arc<Channels>, address: SocketAddr) -> anyhow::Resu
         }
     });
 
-    let route_test = warp::path!("api" / "test").and(warp::get()).map( || {
+    let route_test = warp::path!("api" / "test").and(warp::get()).map(|| {
         let response = "hi";
         warp::reply::json(&response)
     });
 
+    let route_image = warp::path!("api" / "image").and(warp::fs::file("src/images/2.jpg"));
+    //
     /*
      * This is the main thing you will be adding: The new route. You'll need to specificy the route,
      * which basically means specifying the URL that can be used to reach this point of the plane
@@ -61,21 +63,33 @@ pub async fn serve(channels: Arc<Channels>, address: SocketAddr) -> anyhow::Resu
      * warp::fs::file function. The general format of the route will look somewhat similar to the
      * two routes defined above (route_roi and route_telem), but can be shorter and less complex.
      */
-    
 
     let cors = warp::cors()
         .allow_any_origin()
-        .allow_headers(vec!["User-Agent", "Sec-Fetch-Mode", "Referer", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "content-type", "x-requested-with"])
+        .allow_headers(vec![
+            "User-Agent",
+            "Sec-Fetch-Mode",
+            "Referer",
+            "Origin",
+            "Access-Control-Request-Method",
+            "Access-Control-Request-Headers",
+            "content-type",
+            "x-requested-with",
+        ])
         .allow_methods(vec!["GET", "POST", "DELETE", "PUT"]);
 
-    /* 
+    /*
      * You'll also need to make a change to the line below. It basically bundles all the
      * routes together into one object that can be served up for other pieces of software,
      * like the frontend, to access. The format is exactly the same as how you see route_telem
      * being added. The .with(cors) bit at deals with some annoying shenanigans, but basically
      * allows you to access the plane system from the frontend running locally on your computer.
      */
-    let api = route_roi.or(route_telem).or(route_test).with(cors);
+    let api = route_roi
+        .or(route_telem)
+        .or(route_test)
+        .or(route_image)
+        .with(cors);
 
     info!("initialized server");
 
