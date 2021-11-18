a850c783a66d0c6cf5f6f193a533e8775d7bdf67
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-11-18 13:29:08 -0500

    remove container when done

--- docker/user/build.sh
+++ docker/user/build.sh
@@ -7,7 +7,7 @@ ROOT_DIR=$( dirname `dirname ${SCRIPT_DIR}` )
 case "$1" in
 "rpi3-raspbian-v1" | "rpi4-ubuntu-v1")
   echo "building plane system for target $1" 
-  docker run -it -v ${ROOT_DIR}:/app -v ~/.cargo/registry:/home/ccuser/.cargo/registry -v ~/.cargo/git:/home/ccuser/.cargo/git dr.cuair.org/x-compiler:$1
+  docker run -it --rm -v ${ROOT_DIR}:/app -v ~/.cargo/registry:/home/ccuser/.cargo/registry -v ~/.cargo/git:/home/ccuser/.cargo/git dr.cuair.org/x-compiler:$1
   ;;
 "")
   echo "usage: build.sh <target>"
