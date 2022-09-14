edf98018a8a648a96b302b8d8a39ad133716d53f
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-09-14 15:34:38 -0400

    add camera time set

--- crates/main-camera/src/task/mod.rs
+++ crates/main-camera/src/task/mod.rs
@@ -9,10 +9,11 @@ use anyhow::Context;
 pub use control::*;
 pub use download::*;
 pub use event::*;
+use log::{trace, warn};
 use ps_client::ChannelCommandSink;
 use tokio::sync::RwLock;
 
-use crate::{interface, CameraRequest, CameraResponse, MainCameraConfig};
+use crate::{interface::{self, PropertyCode}, CameraRequest, CameraResponse, MainCameraConfig};
 
 pub fn create_tasks(
     config: MainCameraConfig,
@@ -26,6 +27,18 @@ pub fn create_tasks(
     let mut interface =
         interface::CameraInterface::new().context("failed to initialize usb camera interface")?;
     interface.connect().context("failed to set up camera")?;
+
+    trace!("initializing camera");
+
+    let time_str = chrono::Local::now()
+        .format("%Y%m%dT%H%M%S%.3f%:z")
+        .to_string();
+
+    trace!("setting time on camera to '{}'", &time_str);
+
+    if let Err(err) = interface.set(PropertyCode::DateTime, ptp::PtpData::STR(time_str)) {
+        warn!("could not set date/time on camera: {:?}", err);
+    }
 
     let interface = Arc::new(RwLock::new(interface));
 
