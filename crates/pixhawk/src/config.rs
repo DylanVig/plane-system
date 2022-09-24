14a27027732fcfe18e7bad64ee4501a913abd575
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-09-24 14:50:19 -0400

    move in the pixhawk config

---
+++ crates/pixhawk/src/config.rs
@@ -0,0 +1,10 @@
+use std::net::SocketAddr;
+
+use mavlink::MavlinkVersion;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct PixhawkConfig {
+    pub address: SocketAddr,
+    pub mavlink: MavlinkVersion,
+}
