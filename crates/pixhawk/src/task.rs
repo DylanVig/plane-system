02b39f3db8b379010d45fcdc4b081da70958dfa4
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-09-24 15:09:27 -0400

    put the handling code in the pixhawk task

--- crates/pixhawk/src/task.rs
+++ crates/pixhawk/src/task.rs
@@ -3,14 +3,20 @@ use log::*;
 
 use async_trait::async_trait;
 use ps_client::Task;
+use ps_types::{Attitude, Point3D};
 use tokio::select;
 use tokio_util::sync::CancellationToken;
 
-use crate::{PixhawkEvent, PixhawkConfig};
+use mavlink::{
+  ardupilotmega as apm, common,
+};
+
+use crate::{interface::PixhawkInterface, PixhawkConfig, PixhawkEvent};
 
 pub struct EventTask {
-  evt_tx: flume::Sender<PixhawkEvent>,
-  evt_rx: flume::Receiver<PixhawkEvent>,
+    interface: PixhawkInterface,
+    evt_tx: flume::Sender<PixhawkEvent>,
+    evt_rx: flume::Receiver<PixhawkEvent>,
 }
 
 pub fn create_task(config: PixhawkConfig) -> anyhow::Result<EventTask> {
@@ -24,13 +30,43 @@ impl Task for EventTask {
     }
 
     async fn run(self: Box<Self>, cancel: CancellationToken) -> anyhow::Result<()> {
-        let Self {
-            ..
-        } = *self;
+        let Self { mut interface, evt_tx, .. } = *self;
 
         let loop_fut = async move {
             loop {
-              todo!()
+                let message = interface.recv().await?;
+
+                match message {
+                    apm::MavMessage::common(common::MavMessage::GLOBAL_POSITION_INT(data)) => {
+                        let _ = evt_tx.send(PixhawkEvent::Gps {
+                            position: Point3D {
+                                point: geo::Point::new(
+                                    data.lon as f32 / 1e7,
+                                    data.lat as f32 / 1e7,
+                                ),
+                                altitude_msl: data.alt as f32 / 1e3,
+                                altitude_rel: data.relative_alt as f32 / 1e3,
+                            },
+                            // velocity is provided as (North, East, Down)
+                            // so we transform it to more common (East, North, Up)
+                            velocity: (
+                                data.vy as f32 / 100.,
+                                data.vx as f32 / 100.,
+                                -data.vz as f32 / 100.,
+                            ),
+                        });
+                    }
+                    apm::MavMessage::common(common::MavMessage::ATTITUDE(data)) => {
+                        let _ = evt_tx.send(PixhawkEvent::Orientation {
+                            attitude: Attitude::new(
+                                data.roll.to_degrees(),
+                                data.pitch.to_degrees(),
+                                data.yaw.to_degrees(),
+                            ),
+                        });
+                    }
+                    _ => {}
+                }
             }
 
             #[allow(unreachable_code)]
