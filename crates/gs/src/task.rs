2a87aeb73a1c64b189ff29037700f2d009f1caa5
Purit <pb465@cornell.edu>
2022-10-22 11:39:31 -0400

    declared new command type for gs crate

--- crates/gs/src/task.rs
+++ crates/gs/src/task.rs
@@ -1,10 +1,49 @@
+use crate::state::*;
+use crate::{image::ImageClientEvent, Channels};
+use anyhow::Context;
 use async_trait::async_trait;
+use clap::{AppSettings, Subcommand};
+use flume;
+use futures::{select, FutureExt};
 use ps_client::Task;
+use reqwest;
+use serde_json::json;
+use std::{ffi::OsStr, str::FromStr, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+use telemetry::Telemetry;
 use tokio::select;
 use tokio_util::sync::CancellationToken;
 
 // use crate::{gs}
 
-pub struct EventTask {
-    // interface: GsInterface,
+pub enum GsCommand {
+    UploadImage {
+        data: Arc<Vec<u8>>,
+        telemetry: Option<Telemetry>,
+    },
 }
+
+pub type GsCommand = flume::Sender<GsCommand>;
+
+pub struct EventTask {
+    interface: GsInterface,
+    // cmd_rx:
+}
+
+impl EventTask {
+    //pub async fn send_image()
+}
+
+//ground server enum -- send image
+pub struct ImageClientEvent {
+    data: Arc<Vec<u8>>,
+    file: PathBuf,
+    telemetry: Option<Telemetry>,
+}
+
+//flume::Sender
+
+// pub type Command<
