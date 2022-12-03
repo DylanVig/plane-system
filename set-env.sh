0fdf5745f1683cfba12666c3a7ff16133a76b544
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-12-03 14:44:39 -0500

    add logging

--- set-env.sh
+++ set-env.sh
@@ -1,2 +1,2 @@
-export RUST_LOG="plane_system=debug,ps_telemetry=debug,ps_livestream=debug,ps_main_camera=debug,ps_pixhawk=debug,ps_gs=debug,ps_download=debug"
+export RUST_LOG="plane_system=debug,ps_telemetry=debug,ps_livestream=debug,ps_main_camera=debug,ps_pixhawk=debug,ps_gs=debug,ps_download=debug,ps_gimbal=debug"
 export RUST_BACKTRACE=full
