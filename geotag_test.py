b57e5edf6d9eda2a610c8a303b4ceefce915f4b4
Owen Sorber <ons3@cornell.edu>
2023-01-18 16:55:58 -0500

    Update with comments

--- geotag_test.py
+++ geotag_test.py
@@ -61,11 +61,11 @@ with open(json_filename, "rb") as json_file:
   image_lat = telem['pixhawk']['position'][0]['point']['lat']
   image_long = telem['pixhawk']['position'][0]['point']['lon']
   image_alt_msl = telem['pixhawk']['position'][0]['altitude_msl']
-  image_altitude = image_alt_msl - 316.6499938964844 # telem['pixhawk']['position'][0]['altitude_rel']
+  image_altitude = telem['pixhawk']['position'][0]['altitude_rel']
 
-  image_roll = 0 #telem['pixhawk']['attitude'][0]['roll']
-  image_pitch = 0 #telem['pixhawk']['attitude'][0]['pitch']
-  image_yaw = 3.14 #telem['pixhawk']['attitude'][0]['yaw']
+  image_roll = telem['pixhawk']['attitude'][0]['roll']
+  image_pitch = telem['pixhawk']['attitude'][0]['pitch']
+  image_yaw = telem['pixhawk']['attitude'][0]['yaw']
 
 
 
@@ -77,44 +77,52 @@ delta_pixel_y = image_height / 2 - PIXEL_Y
 
 """ GEOTAGGING CALCULATIONS: do not change unless you want to edit the model """
 
-hdi = image_altitude * sensor_width / FOCAL_LENGTH
-vdi = image_altitude * sensor_height / FOCAL_LENGTH
+# Calculate target_dx and target_dy
+# target_dx = meters between target and plane on ground in longitude/x direction
+# target_dy = meters between target and plane on ground in latitude/y direction
 
+# New approach
+
+# Focal length in pixels
 f_pixels = FOCAL_LENGTH * image_width / sensor_width
-r_unit_target_rel_plane_P = 1 / sqrt(f_pixels ** 2 + delta_pixel_x ** 2 + delta_pixel_y ** 2) * np.array([-delta_pixel_x, -delta_pixel_y, -f_pixels])
 
-# DCM - Note that pitch and yaw are negated because of reference frame definition
+# Unit vector of target wrt plane in P frame
+r_unit_target_rel_plane_P = 1 / sqrt(f_pixels ** 2 + delta_pixel_x ** 2 + delta_pixel_y ** 2) * np.array([delta_pixel_x, delta_pixel_y, -f_pixels])
+
+# Calculate DCM - Note that yaw is negated because reference frame definition would indicate positive is CC but positive yaw is clockwise
 C_2_roll = np.matrix([[cos(image_roll), 0, -sin(image_roll)], [0, 1, 0], [sin(image_roll), 0, cos(image_roll)]])
-C_1_pitch = np.matrix([[1, 0, 0], [0, cos(-image_pitch), sin(-image_pitch)], [0, -sin(-image_pitch), cos(-image_pitch)]])
+C_1_pitch = np.matrix([[1, 0, 0], [0, cos(image_pitch), sin(image_pitch)], [0, -sin(image_pitch), cos(image_pitch)]])
 C_3_yaw = np.matrix([[cos(-image_yaw), sin(-image_yaw), 0], [-sin(-image_yaw), cos(-image_yaw), 0], [0, 0, 1]])
 P_C_I = C_2_roll @ C_1_pitch @ C_3_yaw
 I_C_P = np.transpose(P_C_I)
 
+# Transform into unit vector of target wrt plane in I frame
 r_unit_target_rel_plane_I = np.transpose(I_C_P @ r_unit_target_rel_plane_P)
-r_target_plane_I = image_altitude / r_unit_target_rel_plane_I[2, 0] * r_unit_target_rel_plane_I
+
+# Find actual vector of target wrt plane; first and second entries are target_dx and target_dy
+r_target_plane_I = image_altitude / abs(r_unit_target_rel_plane_I[2, 0]) * r_unit_target_rel_plane_I
 target_dx = r_target_plane_I[0, 0]
 target_dy = r_target_plane_I[1, 0]
 
-# meters between target and plane on ground in longitude/x direction 
+
+# Old approach
 target_dx_old = image_altitude * (
 							tan(-image_roll + fov_x * delta_pixel_x / image_width) * cos(image_yaw)
         			  	  + tan(image_pitch + fov_y * delta_pixel_y / image_height) * sin(image_yaw)
         			   )
-
-# meters between target and plane on ground in latitude/y direction 
 target_dy_old = image_altitude * (
 							tan(-image_roll + fov_x * delta_pixel_x / image_width) * sin(-image_yaw)
         			  	  + tan(image_pitch + fov_y * delta_pixel_y / image_height) * cos(image_yaw)
         			   )
 
+
 print(image_roll, image_pitch, image_yaw)
 print(target_dx, target_dx_old)
 print(target_dy, target_dy_old)
 
-distance_to_target = sqrt(target_dx ** 2 + target_dy ** 2) # meters
+# Computations for conversion back into lat/long using inverse haversine
+distance_to_target = sqrt(target_dx_old ** 2 + target_dy ** 2) # meters
 direction_to_target = pi / 2.0 - atan2(target_dy, target_dx) # radians
-
-#print(distance_to_target)
 
 # Returns new latitude and longitude in DEGREES
 def inverse_haversine(ilat, ilong, dist, dir):
@@ -129,5 +137,5 @@ def inverse_haversine(ilat, ilong, dist, dir):
 new_lat, new_long = inverse_haversine(image_lat * pi / 180, image_long * pi / 180, distance_to_target, direction_to_target)
 
 # Prints the predicted lat/long of the image
-print(new_lat)
-print(new_long)
+print(new_lat, image_lat, new_lat - image_lat)
+print(new_long, image_long, new_long - image_long)
