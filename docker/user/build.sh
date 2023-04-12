38f9e2df387bc265053c3da1263721778702fb95
Ibiyemi Abiodun <ibiyemi@intulon.com>
2023-04-12 02:09:52 -0400

    missing `TARGET_RUST` arg in Dockerfile

--- docker/user/build.sh
+++ docker/user/build.sh
@@ -4,23 +4,23 @@
 
 set -euxo pipefail
 
-DOCKER="${DOCKER:-podman}"
+RUNTIME="${RUNTIME:-podman}"
 
 # copied from https://stackoverflow.com/a/246128/3508956
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$( dirname `dirname ${SCRIPT_DIR}` )
 
 if ! podman volume exists plane-system-cargo; then 
-  $DOCKER volume create plane-system-cargo
+  $RUNTIME volume create plane-system-cargo
 fi
 if ! podman volume exists plane-system-build; then 
-  $DOCKER volume create plane-system-build
+  $RUNTIME volume create plane-system-build
 fi
 
 case "$1" in
-"rpi3-raspbian/v1" | "rpi4-ubuntu/v1")
+"rpi3-raspbian" | "rpi4-ubuntu")
   echo "building plane system for target $1" 
-  $DOCKER run -it --rm  -v ${ROOT_DIR}:/app -v plane-system-build:/app/target -v plane-system-cargo:/home/ccuser/.cargo/registry dr.cuair.org/x-compiler/$1:latest
+  $RUNTIME run -it --rm  -v ${ROOT_DIR}:/app -v plane-system-build:/app/target -v plane-system-cargo:/home/ccuser/.cargo/registry dr.cuair.org/x-compiler/$1:v1
   ;;
 "")
   echo "usage: build.sh <target>"
